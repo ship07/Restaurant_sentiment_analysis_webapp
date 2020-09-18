144f45395f1d25dc9bc3c759eedaf5a4c5e82a50
ship07 <shipra.singh560@gmail.com>
2020-09-19 00:39:27 +0100

    changes done

--- app.py
+++ app.py
@@ -10,23 +10,10 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-dataset=pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3)
-corpus=[]
-model=pickle.load(open('./model/nbc_model.pickle','rb'))
-def new_prediction(new_review):
-    for i in range(0,1000):
-        review=re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
-        review=review.lower()
-        review=review.split()
-        ps=PorterStemmer()
-        all_stopwords=stopwords.words('english')
-        #all_stopwords.remove('not')
-        review=[ps.stem(word) for word in review if word not in set(all_stopwords)]
-        review=" ".join(review)
-        corpus.append(review)
-    cv=CountVectorizer()
-    cv.fit_transform(corpus)
 
+model=pickle.load(open('./model/nbc_model.pickle','rb'))
+cv=pickle.load(open('./model/counter_vector.pkl','rb'))
+def new_prediction(new_review):
     review=re.sub('[^a-zA-Z]',' ',new_review)
     review=review.lower()
     review=review.split()
@@ -36,7 +23,9 @@ def new_prediction(new_review):
     review=[ps.stem(word) for word in review if word not in set(all_stopwords)]
     review=" ".join(review)
     review=[review]
-    p=model.predict(cv.transform(review).toarray())
+    new_X=cv.transform(review).toarray()
+    new_X=new_X.reshape(1,new_X.size)
+    p=model.predict(new_X)
     return p[0]
 
 
